7408a77f90ef37693945c629dfafa22d83f15ef8
bmatiascxv <matiaben@sheridancollege.ca>
2020-09-25 02:19:45 -0400

    Fixed syntax errors

--- Java Projects/Accounting/Accoutning/src/Bank/Savings.java
+++ Java Projects/Accounting/Accoutning/src/Bank/Savings.java
@@ -7,51 +7,49 @@ Program: Savings.java
 
 public class Savings extends Account {
 
-    private double overdraft, interest_rate = 0, interest;
+    private double overdraft, interest_rate = 0;
 
     Savings() {
-        // Setting parent class values to empty values //
-        super(0, "", 0);
+      // Setting parent class values to empty values //
+      super(0, "", 0);
     }
 
-    Savings(int account_ID, double balance, double overdraft) {
-        // Initializing values for parent class //
-        super(account_ID, "Savings", balance);
-        this.overdraft = overdraft;
+    Savings(final int account_ID, final double balance, final double overdraft) {
+      // Initializing values for parent class //
+      super(account_ID, "Savings", balance);
+      this.overdraft = overdraft;
     }
 
     public double getOverdraft() {
-        return overdraft;
+      return overdraft;
     }
 
-    public void setOverdraft(double overdraft) {
-        if (overdraft >= 0) {
-            this.overdraft = overdraft;
-        } else {
-            throw new IllegalArgumentException("Invalid Overdraft Amount");
-        }
+    public void setOverdraft(final double overdraft) {
+      if (overdraft >= 0) {
+        this.overdraft = overdraft;
+      } else {
+        throw new IllegalArgumentException("Invalid Overdraft Amount");
+      }
     }
 
     public double getInterest_rate() {
-        return interest_rate;
+      return interest_rate;
     }
 
-    public void setInterest_rate(double interest_rate) {
-        if (interest_rate >= 0) {
-            this.interest_rate = interest_rate;
-        } else {
-            throw new IllegalArgumentException("Invalid Interest Rate");
-        }
+    public void setInterest_rate(final double interest_rate) {
+      if (interest_rate >= 0) {
+        this.interest_rate = interest_rate;
+      } else {
+        throw new IllegalArgumentException("Invalid Interest Rate");
+      }
     }
 
     public void addInterest() {
-
-        this.interest = this.balance * this.interest_rate;
     }
 
-    public void withdraw(double withdraw_amount) {
+    public void withdraw(final double withdraw_amount) {
 
-        double withdraw_limit = balance + overdraft;
+        final double withdraw_limit = balance + overdraft;
 
         if (withdraw_amount <= withdraw_limit && withdraw_amount >= 0) {
             balance -= withdraw_amount;
